5fdddabf557b5031467815a587acc681c71b2171
Edmond Chaussidon <edmondchau@sfr.fr>
2019-10-16 15:32:27 +0200

    premier essai

---
+++ Juckes/README.md
@@ -0,0 +1,5 @@
+Exemple netcdf4 sur python 
+
+* <http://pyhogs.github.io/intro_netcdf4.html>
+
+* <http://schubert.atmos.colostate.edu/~cslocum/netcdf_example.html>
---
+++ Juckes/data_reader.py
@@ -0,0 +1,9 @@
+import numpy as np
+import netCDF4 as nc4
+
+data = nc4.Dataset('Data/PA_THETA_Z_2PVU_2016010100-2016013100.nc','r')
+
+print(" ")
+print(data.variables.keys())
+print(" ")
+print(data.dimensions.keys())
