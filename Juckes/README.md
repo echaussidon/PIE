46a1b26b4fc22265aa12bb284d92d3cb8f0956d7
Edmond Chaussidon <edmondchau@sfr.fr>
2019-11-05 16:06:04 +0100

    add ressource

--- Juckes/README.md
+++ Juckes/README.md
@@ -1,5 +1,9 @@
-Exemple netcdf4 sur python 
+Exemple netcdf4 sur python
 
 * <http://pyhogs.github.io/intro_netcdf4.html>
 
 * <http://schubert.atmos.colostate.edu/~cslocum/netcdf_example.html>
+
+* <https://unidata.github.io/netcdf4-python/netCDF4/index.html>
+
+* <https://joehamman.com/2013/10/12/plotting-netCDF-data-with-Python/>
--- Juckes/data_reader.py
+++ Juckes/data_reader.py
@@ -3,7 +3,18 @@ import netCDF4 as nc4
 
 data = nc4.Dataset('Data/PA_THETA_Z_2PVU_2016010100-2016013100.nc','r')
 
+
 print(" ")
 print(data.variables.keys())
 print(" ")
-print(data.dimensions.keys())
+
+
+GP = data.variables['GP_GDS0_PVL']
+POT = data.variables['POT_GDS0_PVL']
+lat = data.variables['g0_lat_1']
+long = data.variables['g0_lon_2']
+
+print(GP.shape)
+print(POT.shape)
+print(lat.shape)
+print(long.shape)
