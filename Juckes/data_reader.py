14d6c54561b9cd290e8c6c39e37f78c5c768929f
Edmond Chaussidon <edmondchau@sfr.fr>
2019-11-06 11:36:18 +0100

    nouvelle version

--- Juckes/data_reader.py
+++ Juckes/data_reader.py
@@ -16,16 +16,25 @@ long = data.variables['g0_lon_2']
 initial_time = data.variables['initial_time0_hours']
 
 for i in tqdm(range(initial_time[:].size)):
-    plt.figure(figsize=(10,6))
+    plt.figure(figsize=(7.5,7.8))
 
-    ax = plt.axes(projection=crs.PlateCarree())
-    plt.contourf(long[:], lat[:], GP[i,:,:], 80, transform=crs.PlateCarree(), cmap=plt.cm.viridis)
-
-    cbar = plt.colorbar(orientation='horizontal', pad = 0.05)
-    cbar.ax.set_xlabel("GP [{}]".format(GP.units))
+    ax1 = plt.subplot(2, 1, 1, projection=crs.PlateCarree())
+    fig1 = ax1.contourf(long[:], lat[:], GP[i,:,:], 80, transform=crs.PlateCarree(), cmap=plt.cm.viridis)
+    cbar1 = plt.colorbar(fig1, ax=ax1, orientation='horizontal', pad = 0.05)
+    ax1.set_aspect('auto', adjustable=None)
+    cbar1.ax.set_xlabel("GP [{}]".format(GP.units))
+    ax1.add_feature(cartopy.feature.COASTLINE)
+    ax1.add_feature(cartopy.feature.BORDERS, linestyle=':', alpha=0.5)
     plt.title(r"$t = {} h$".format(initial_time[i]))
 
-    ax.add_feature(cartopy.feature.COASTLINE)
-    ax.add_feature(cartopy.feature.BORDERS, linestyle=':', alpha=0.5)
+    ax2 = plt.subplot(2, 1, 2, projection=crs.PlateCarree())
+    fig2 = ax2.contourf(long[:], lat[:], POT[i,:,:], 80, transform=crs.PlateCarree(), cmap=plt.cm.viridis)
+    cbar2 = plt.colorbar(fig2, ax=ax2, orientation='horizontal', pad = 0.05)
+    ax2.set_aspect('auto', adjustable=None)
+    cbar2.ax.set_xlabel("POT [{}]".format(POT.units))
+    ax2.add_feature(cartopy.feature.COASTLINE)
+    ax2.add_feature(cartopy.feature.BORDERS, linestyle=':', alpha=0.5)
 
-    plt.savefig("Result/GP_{}.png".format(i), transparent=True)
+    #plt.show()
+
+    plt.savefig("Result/data_t{}.png".format(i), transparent=True)
