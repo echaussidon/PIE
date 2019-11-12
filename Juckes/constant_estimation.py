3be7fadaa66f290efa766c6be3be7a89ad685ee0
Edmond Chaussidon <edmondchau@sfr.fr>
2019-11-12 15:57:04 +0100

    ajout mean constante

--- Juckes/constant_estimation.py
+++ Juckes/constant_estimation.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import netCDF4 as nc4
 import cartopy as cartopy
 import cartopy.crs as crs
+import tqdm as tqdm
 
 import utils
-
 
 data = nc4.Dataset('Data/PA_THETA_Z_2PVU_2016010100-2016013100.nc','r')
 
@@ -14,41 +14,33 @@ GP = data.variables['GP_GDS0_PVL']
 POT = data.variables['POT_GDS0_PVL']
 LAT = data.variables['g0_lat_1']
 LON = data.variables['g0_lon_2']
-initial_time = data.variables['initial_time0_hours']
 
+mean_GP = np.mean(GP, axis=0) #moyenne temporelle du geopotentiel
+mean_POT = np.mean(POT, axis=0) #moyenne temporelle de la temperature potentielle
 
-#Obtention de la hauteur moyenne et de la température potentielle moyenne de la tropopause
-#On fait une moyenne temporelle (GP et POT ne sont pas uniforme selon LAT et LON)
+dGP = np.subtract(GP, mean_GP) #anomalie du geopotentiel
+dZ = dGP/9.81 #anomalie de la hauteur de la tropopause
+dPOT  = np.subtract(POT, mean_POT) #anomalie de la temperature potentielle
 
-mean_GP = np.mean(GP, axis=0)
-mean_POT = np.mean(POT, axis=0)
+Constante = dZ/dPOT
 
+#Constante[np.abs(Constante) > 1000] = False
 
-utils.plot_2(LON, LAT, mean_GP, mean_POT, "GP [{}]".format(GP.units), "POT [{}]".format(POT.units), title="Moyenne temporelle du Geopotentielle et de la température potentielle")
+mean_t_Constante = np.mean(Constante, axis=0)
 
+mean_lon_lat_t_Constate = np.mean(mean_t_Constante)
+std_dev = np.std(mean_t_Constante)
 
-"""
-# évolution de la température selon la latitude moyennée sur toutes les longitudes
-mean_geopot_over_long=np.zeros((361))
-for k in range(0,720):
-    mean_geopot_over_long = mean_geopot_over_long + np.array(mean_geopot[:,k])
-mean_geopot_over_long = mean_geopot_over_long/720
+print(mean_lon_lat_t_Constate, std_dev)
 
-#plot du géopotentiel moyen en fonction de la latitude
-plt.plot(lat, mean_geopot_over_long)
-plt.show()
+###############
+## Affichage ##
+###############
 
+#utils.plot_2(LON, LAT, mean_GP, mean_POT, "GP [{}]".format(GP.units), "POT [{}]".format(POT.units), title="Moyenne temporelle du Geopotentielle et de la température potentielle", save=True, savename="Result/mean_data")
 
-# Calcul des anomalies de températue et de la variation de hauteur de la tropopause
-temp_anomaly = data.variables['POT_GDS0_PVL'][:,:,:] #l'anomalie en température de la tropopause
-dz_trop  = data.variables['GP_GDS0_PVL'][:,:,:]     #la perturbation de hauteur de la tropopause
-for k in range(0,31):
-    temp_anomaly[k,:,:] = temp_anomaly[k,:,:]-mean_pot_temp
-    dz_trop[k,:,:] = dz_trop[k,:,:]-mean_geopot
-dz_trop = dz_trop/9.81    #on se ramène à une altitude
+#for t in tqdm.tqdm(range(0,31)):
+#    utils.plot_2(LON, LAT, dGP[t], dPOT[t], "dGP [{}]".format(GP.units), "dPOT [{}]".format(POT.units), title="Anomalie du Geopotentielle et de la température potentielle", save=True, savename="Result/Anomalie/Anomalie_t{}".format(t), show=False)
+#    utils.plot(LON, LAT, Constante[t], "Constante [m/K]", title="verification de (9.3)", save=True, savename="Result/Constante/constante_t{}".format(t), show=False)
 
-Const_tab = dz_trop/temp_anomaly   #array contenant les rapports dz sur anomalie
-Const = np.mean(Const_tab[:,:,:])
-std_dev = np.std(Const_tab[:,:,:])
-
-"""
+#utils.plot(LON, LAT, mean_t_Constante[:,:], "Constante [m/K]", title="moyenne temporelle de la Constante", save=True, savename="Result/mean_Constante", show=False)
