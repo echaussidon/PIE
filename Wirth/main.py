79121f69dfd5ca9c2cfe93cb20d606addd52e5cb
Augustin Tommasini <Augustin372@github.com>
2019-11-25 16:08:11 +0100

    modification de la fonction theta_initialization

--- Wirth/main.py
+++ Wirth/main.py
@@ -68,16 +68,80 @@ for i in range(Nx):
 def theta_initialization(k):
     """Initialisation de la température
     k=1: sinusoïdale
-    k=2:
+    k=2: allongée verticalement
+    k=3: allongée horizontalement
     """
     theta = np.zeros((Nx,Ny))                                   # les inconnues à l'instant t
-    for i in range(Nx):
-        for j in range(Ny):
-            if(k==1):
+    if(k==1):
+        for i in range(Nx):
+            for j in range(Ny):
                 theta[i][j] = 20 * np.sin(np.pi * i / Nx)*np.sin(np.pi * j / Ny)
-            #elif(k==2):
-
-    return theta
+    
+    elif(k==2 or k==3):
+        a_x = 0.1
+        a_y = 0.3
+        delta = 0.1  
+        L = 0.55
+        l = 0
+        
+        #définition de theta_x
+        theta_x = np.zeros(Nx)
+        u = 0
+        v = int(a_x*Nx)
+        for i in range(u,v):
+            theta_x[i]=0
+        u = v
+        v = int((a_x+delta)*Nx)
+        for i in range(u,v):
+            theta_x[i]=(1+np.sin(np.pi*(i-u)/(v-u)-np.pi/2))/2
+        u = v
+        v = int((a_x+delta+L)*Nx)
+        for i in range(u,v):
+            theta_x[i]=1
+        u = v
+        v = int((a_x+delta+L+delta)*Nx)
+        for i in range(u,v):
+            theta_x[i]=(1-np.sin(np.pi*(i-u)/(v-u)-np.pi/2))/2
+        u = v
+        v = Nx
+        for i in range(u,v):
+            theta_x[i]=0
+        
+        #définition de theta_y
+        theta_y = np.zeros(Ny)
+        u = 0
+        v = int(a_y*Ny)
+        for i in range(u,v):
+            theta_y[i]=0
+        u = v
+        v = int((a_y+delta)*Ny)
+        for i in range(u,v):
+            theta_y[i]=(1+np.sin(np.pi*(i-u)/(v-u)-np.pi/2))/2
+        u = v
+        v = int((a_y+delta+l)*Ny)
+        for i in range(u,v):
+            theta_y[i]=1
+        u = v
+        v = int((a_y+delta+l+delta)*Ny)
+        for i in range(u,v):
+            theta_y[i]=(1-np.sin(np.pi*(i-u)/(v-u)-np.pi/2))/2
+        u = v
+        v = Ny
+        for i in range(u,v):
+            theta_y[i]=0
+        
+        
+        #définition de theta 
+        if(k==2):
+            for i in range(Nx):
+                for j in range(Ny):
+                    theta[i][j] = -20 * theta_x[i] * theta_y[j]
+        elif(k==3):
+            for i in range(Nx):
+                for j in range(Ny):
+                    theta[i][j] = -20 * theta_y[i] * theta_x[j]
+            
+    return theta 
 
 theta = theta_initialization(1)
 thetatp1 = np.copy(theta)                                   # les inconnues à instant t+dt (à calculer dans la boucle)
