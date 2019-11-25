fa7e6b58dd47515e498647488a043070b87ff206
Edmond Chaussidon <edmondchau@sfr.fr>
2019-11-25 19:01:16 +0100

    correction petit probelme

--- Juckes/constant_estimation.py
+++ Juckes/constant_estimation.py
@@ -40,18 +40,15 @@ print("\nValeur moyenne de la Constante = {}, standard deviation = {} \n".format
 ###############
 
 #utils.plot_2(LON, LAT, mean_GP, mean_POT, "GP [{}]".format(GP.units), "POT [{}]".format(POT.units), title="Moyenne temporelle du Geopotentielle et de la température potentielle", save=True, savename="Result/mean_data")
-<<<<<<< HEAD
 #
 #for t in range(0,31):
 #    utils.plot_2(LON, LAT, dGP[t], dPOT[t], "dGP [{}]".format(GP.units), "dPOT [{}]".format(POT.units), title="Anomalie du Geopotentielle et de la température potentielle", save=True, savename="Result/Anomalie/Anomalie_t{}".format(t), show=False)
 #    utils.plot(LON, LAT, Constante[t], "Constante [m/K]", title="verification de (9.3)", save=True, savename="Result/Constante/constante_t{}".format(t), show=False)
 #
 #utils.plot(LON, LAT, mean_t_Constante[:,:], "Constante [m/K]", title="moyenne temporelle de la Constante", save=True, savename="Result/mean_Constante", show=False)
-=======
 
 for t in tqdm.tqdm(range(0,31)):
     #utils.plot_2(LON, LAT, dGP[t], dPOT[t], "dGP [{}]".format(GP.units), "dPOT [{}]".format(POT.units), title="Anomalie du Geopotentielle et de la température potentielle", save=True, savename="Result/Anomalie/Anomalie_t{}".format(t), show=False)
     utils.plot(LON, LAT, Constante_mask[t], "Constante [m/K]", title="verification de (9.3)", save=True, savename="Result/Constante/constante_t{}".format(t), show=False)
 
 utils.plot(LON, LAT, mean_t_Constante[:,:], "Constante [m/K]", title="Moyenne totale Constante = {}, standard deviation = {}".format(mean_lon_lat_t_Constante, std_dev), save=True, savename="Result/mean_Constante", show=False)
->>>>>>> 2b5cb7044ed1a294e587dd988cf637f7544cb365
